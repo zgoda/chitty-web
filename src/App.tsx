2bbfa2d8bc46644aa4a08e6ccb6f50ecb5e190be
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-03-11 22:46:32 +0100

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { h, JSX } from 'preact';
+import { useLang, useTitle, useMeta } from 'hoofd/preact';
 
 const App = ((): JSX.Element => {
+
+  useLang('en');
+  useTitle('Chitty chat');
+  useMeta({ name: 'author', content: 'Jarek Zgoda' });
+
   return (
     <div class="container grid-md">
       <h1>Chitty</h1>
+      <div class="columns">
+        <div class="column col-9">
+          Chat
+        </div>
+        <div class="column col-3">
+          Topics
+        </div>
+      </div>
     </div>
   );
 });
